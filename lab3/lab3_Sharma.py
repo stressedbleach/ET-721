0a310c4ec086c8097d7ca17996221412ba5d8743
stressedbleach <simi.sharma025@gmail.com>
2024-09-09 13:28:21 -0400

    example 2 + 3; loops + break statements

--- lab3/lab3_Sharma.py
+++ lab3/lab3_Sharma.py
@@ -36,3 +36,43 @@ else:
     
     
 print(f"Your final grade for the class is {final}, and your gpa is {gpa}.")
+
+
+print ("---------- Example 2: loops ----------")
+
+secret = 8
+
+userguess = int(input("Guess the secret number between 1 and 10: "))
+
+while not (secret == userguess):
+    userguess = int(input("wrong guess...womp womp loser :( \n Guess again: "))
+
+print(f"Congrats! {userguess} is the right number!")
+
+print("---------- Example 3: loops, break statements ----------")
+
+balance = 1000
+widthdraw = 0
+deposit = 0
+
+while True:
+    userinput = input("Do you want to widthdraw (1), or deposit (2) your money today? ")
+    if userinput == '1':
+        w_amount = int(input("How much do you want to widthdraw? "))
+        if w_amount>balance:
+            print(f"Insufficient funds. You cannot widthdraw more than your balance, {balance}")
+        else:
+            balance -= w_amount
+            print(f"Your new balance is {balance}.")
+    elif userinput == '2':
+        d_amount = int(input("How much do you want to deposit? "))
+        balance += d_amount
+        print(f"Your new balance is {balance}.")
+    else: 
+        print("Invalid selection!")
+    
+    choice = input("Would you like to do another transaction? (Y/N) ")
+    if not(choice=='Y' or choice == 'y'):
+        break
+
+print("Thank you for banking with us!")
